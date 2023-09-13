b18b36386f48e5b8239eaecf4a55c9f048e3c451
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-13 19:48:16 +0300

    task2 upload

--- homework1/main.c
+++ homework1/main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+int getNum(void)
+{
+    int number = 0;
+    char symbol = 0;
+
+    while (true)
+    {
+        int input = scanf_s("%d%c", &number, &symbol);
+        if (input == 2 && symbol == '\n')
+        {
+            break;
+        }
+        else
+        {
+            printf("\nInvalid input format\nTry again: ");
+        }
+        while ((symbol = getchar()) != '\n' && symbol != EOF);
+    }
+    return number;
+}
 
 int main()
 {
-	print("Hello, world!");
+    int number1 = 0;
+    printf("Enter the first number: ");
+    number1 = getNum();
+
+    int number2 = 0;
+    printf("\nEnter the second number: ");
+    number2 = getNum();
+
+    printf("\nInitial numbers: %d, %d\n", number1, number2);
+    number1 += number2;
+    number2 = number1 - number2;
+    number1 = number1 - number2;
+    printf("The resulting numbers: %d, %d\n", number1, number2);
 }
