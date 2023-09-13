bd95b168e9f0f008180fb5d7ad433195d20ac741
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-13 20:07:29 +0300

    task6 upload

--- homework1/main.c
+++ homework1/main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int getNum(void)
+{
+	int number = 0;
+	char symbol = 0;
+
+	while (true)
+	{
+		int input = scanf_s("%d%c", &number, &symbol);
+		if (input == 2 && symbol == '\n' && number > 0)
+		{
+			break;
+		}
+		else
+		{
+			printf("\nInvalid input format\nTry again: ");
+		}
+		while ((symbol = getchar()) != '\n' && symbol != EOF);
+	}
+	return number;
+}
 
 int main()
 {
-	print("Hello, world!");
+	int limit = 0;
+	printf("Enter the limit: ");
+	limit = getNum();
+
+	printf("\nPrime numbers not exceeding a given number: ");
+	for (int number = 2; number < limit + 1; number++)
+	{
+		bool has_divisors = false;
+		for (int divider = 2; divider < sqrt(number) + 1; divider++) {
+			if (number % divider == 0) {
+				has_divisors = true;
+				break;
+			}
+		}
+		if (!has_divisors) {
+			printf("%d ", number);
+		}
+	}
+	printf("\n");
 }
