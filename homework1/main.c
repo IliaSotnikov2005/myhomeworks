309df89942b16ac918f1480943b58208c620e57a
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-13 20:04:44 +0300

    task5 upload

--- homework1/main.c
+++ homework1/main.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main()
 {
-	print("Hello, world!");
+	char string[80] = { 0 };
+	printf("Enter a bracket sequence of no more than 80 characters long: ");
+	scanf_s("%s", string, 80);
+	const int stringSize = strlen(string);
+
+	char openBracket = '(', closedBracket = ')';
+	if (strstr("{", string) || strstr("}", string))
+	{
+		openBracket = '{';
+		closedBracket = '}';
+	}
+	if (strstr("[", string) || strstr("]", string))
+	{
+		openBracket = '[';
+		closedBracket = ']';
+	}
+
+	int bracketBalance = 0;
+	bool sequenceIsCorrect = true;
+	for (int i = 0; i < stringSize; i++)
+	{
+		if ((string[i] != openBracket) && (string[i] != closedBracket))
+		{
+			printf("\nError: different brackets are used or there are extraneous characters in the sequence\n");
+			sequenceIsCorrect = false;
+			break;
+		}
+		if (bracketBalance < 0)
+		{
+			sequenceIsCorrect = false;
+			break;
+		}
+		if (string[i] == openBracket)
+		{
+			bracketBalance++;
+		}
+		if (string[i] == closedBracket)
+		{
+			bracketBalance--;
+		}
+	}
+	if (sequenceIsCorrect && bracketBalance == 0)
+	{
+		printf("\nThe bracket sequence is correct\n");
+	}
+	else
+	{
+		printf("\nThe bracket sequence is incorrect\n");
+	}
 }
