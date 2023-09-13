8671372043fc9dd46de7a02d9a435135dc268992
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-13 19:56:34 +0300

    task4 upload

--- homework1/main.c
+++ homework1/main.c
@@ -2,5 +2,18 @@
 
 int main()
 {
-	print("Hello, world!");
+	int numberOfAmounts[28] = { 0 };
+	for (int number = 0; number < 1000; number++)
+	{
+		int sumOfDigits = number / 100 + number % 100 / 10 + number % 10;
+		numberOfAmounts[sumOfDigits]++;
+	}
+
+	int numberOfHappyTickets = 0;
+	for (int sumOfDigits = 0; sumOfDigits < 28; sumOfDigits++)
+	{
+		numberOfHappyTickets += numberOfAmounts[sumOfDigits] * numberOfAmounts[sumOfDigits];
+	}
+
+	printf("Number of lucky tickets = %d\n", numberOfHappyTickets);
 }
