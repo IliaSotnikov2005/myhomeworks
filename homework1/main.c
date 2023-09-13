3d80068e405b91d9b1f2daeb8cdedc60aba63d2b
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-13 20:09:24 +0300

    task7 upload

--- homework1/main.c
+++ homework1/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-	print("Hello, world!");
+	char originalString[80] = { 0 };
+	printf("Enter an original string: ");
+	scanf_s("%s", originalString, 80);
+
+	char substring[80] = { 0 };
+	printf("\nEnter an substring: ");
+	scanf_s("%s", substring, 80);
+	const int substringSize = strlen(substring);
+
+	int count = 0;
+	char* startOfTheSearch = originalString;
+	while ((startOfTheSearch = strstr(startOfTheSearch, substring)) != NULL)
+	{
+		count++;
+		startOfTheSearch += substringSize;
+	}
+
+	printf("\nThe string %s enters the original string %d times\n", substring, count);
 }
