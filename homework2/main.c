5f647e72af38806978366287abbaff28eadacdc7
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-14 21:59:30 +0300

    homework2 task1

--- homework2/main.c
+++ homework2/main.c
@@ -1,45 +1,75 @@
 #include <stdio.h>
+#include  <time.h>
 
-int recursiveExponentiation(int number, int power)
+#define NUMBER_OF_FIBONACCI_NUMBERS 45
+
+int recursiveFibonacci(int number)
 {
-	if (power == 0)
+	if (number < 1)
+	{
+		return 0;
+	}
+	if (number == 1 || number == 2)
 	{
 		return 1;
 	}
-	if (power % 2 != 0)
-	{
-		return number * recursiveExponentiation(number, power - 1);
-	}
-	if (power % 2 == 0)
-	{
-		int b = recursiveExponentiation(number, power / 2);
-		return b * b;
-	}
+	return recursiveFibonacci(number - 1) + recursiveFibonacci(number - 2);
 }
 
-int linearExponentiation(int number, int power)
+int linearFibonacci(int number)
 {
-	int result = 1;
-	for (int i = 0; i < power; i++)
+	int number1 = 1, number2 = 1, number3 = 2;
+	if (number < 1)
 	{
-		result *= number;
+		return 0;
 	}
-	return result;
+	if (number == 1 || number == 2)
+	{
+		return 1;
+	}
+	else
+	{
+		for (int i = 3; i < number; i++)
+		{
+			number1 = number2;
+			number2 = number3;
+			number3 = number1 + number2;
+		}
+		return number3;
+	}
 }
 
 int main()
 {
-	printf("Table of results for numbers from 2 to 7 raised to a power from 0 to 10\n\n\n");
-	for (int number = 2; number < 8; number++)
+	printf("The fibonacci sequence constructed by a linear algorithm:\n");
+	float runtimesLinear[NUMBER_OF_FIBONACCI_NUMBERS] = { 0 };
+	for (int number = 1; number < NUMBER_OF_FIBONACCI_NUMBERS + 1; number++)
 	{
-		printf("Number %d\nLinear    Recursive    Matching\n", number);
-		for (int power = 0; power <= 10; power++)
-		{
-			int resultOfLinearExponentiation = linearExponentiation(number, power);
-			int resultOfRecursiveExponentiation = recursiveExponentiation(number, power);
-			printf("%9d    %9d    %9d\n", resultOfLinearExponentiation, resultOfRecursiveExponentiation, resultOfLinearExponentiation == resultOfRecursiveExponentiation);
-		}
-		printf("\n\n");
+		clock_t start = clock();
+		printf("%d ", linearFibonacci(number));
+		clock_t end = clock();
+		runtimesLinear[number - 1] = (end - start) / CLOCKS_PER_SEC;
 	}
-	
+	printf("\n\n");
+
+	printf("The fibonacci sequence constructed by a recursive algorithm:\n");
+	float runtimesRecursive[NUMBER_OF_FIBONACCI_NUMBERS] = { 0 };
+	for (int number = 1; number < NUMBER_OF_FIBONACCI_NUMBERS + 1; number++)
+	{
+		clock_t start = clock();
+		printf("%d ", recursiveFibonacci(number));
+		clock_t end = clock();
+		runtimesRecursive[number - 1] = (end - start);
+	}
+	printf("\n\n");
+
+	for (int i = 0; i < NUMBER_OF_FIBONACCI_NUMBERS - 1; i++)
+	{
+		float advantage = (runtimesRecursive[i] - runtimesLinear[i]) / CLOCKS_PER_SEC;
+		if (advantage > 0.5)
+		{
+			printf("When calculating %d fibonacci number linear algoritm is faster then recursive by %.5f seconds\n", i + 1, advantage);
+		}
+	}
 }
+
